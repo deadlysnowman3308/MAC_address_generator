60bc7c0da06623fe2f4166fc7a863b67779c767c
Aniket Dinda <38289183+deadlysnowman3308@users.noreply.github.com>
2020-11-06 19:41:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+  
+# Author: Aniket Dinda
+# Site: https://hackingivla.wordpress.com
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from randmac import RandMac
+import os, sys, time
+
+def start():
+    pass
+    start_time = time.time()
+    try:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        num = int(input("\n\n\tEnter an number: "))
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Number of generated keys: ", num)
+        print("\n")
+        for _ in range(0, num):
+            example_mac = "00-00-00-00-00-00"
+            generated_mac = RandMac(example_mac)            
+            print("[+] Generated 6-byte mac address: ",generated_mac)
+            
+        end = time.time()
+        print("\n\n\t[*] Total time elapsed:>  ", end - start_time)
+            
+    except ValueError:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n\n\n\t[/] Input number only...")  
+        print("\n\n\n\t[*] Try Agian [*]")
+        time.sleep(2)
+        start()
+    except KeyboardInterrupt:
+        print("[+] CTRL+C detected [+]")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        sys.exit()
+start()
+input("\n\n\n\tPress enter for exit")
+os.system('cls' if os.name == 'nt' else 'clear')
+sys.exit()
